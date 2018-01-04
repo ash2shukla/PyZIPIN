a315f666c1d0a6468a138949195a0578e649d9c1
Ashish Shukla <ash2shukla@gmail.com>
2018-01-05 03:57:17 +0530

    Added build 0.1

---
+++ build/lib/PyZIPIN/ZIPDecode.py
@@ -0,0 +1,72 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .createDB import ZIP, Base
+from json import dumps
+from os import path
+
+DB_URI = 'sqlite:///'+path.join(path.dirname(path.abspath(__file__)), "ZIPInfo.db")
+
+engine = create_engine(DB_URI)
+Base.metadata.bind = engine
+DBSession = sessionmaker()
+DBSession.bind = engine
+_session = DBSession()
+
+# Define the retrieving functions here
+
+def decode(zip_code,all_results=False):
+	'''
+		for a ZIP returns:
+
+		'officename'
+		'pincode'
+		'officetype'
+		'Deliverystatus'
+		'divisionname'
+		'regionname'
+		'circlename'
+		'taluk'
+		'districtname'
+		'statename'
+
+		officetype has 3 values:
+		H.O. Head Post Office
+		B.O. Branch Post Office
+		S.O. Sub Post Office
+	'''
+	try:
+		int(zip_code)
+	except:
+		return None
+
+	if all_results :
+		retval = []
+		results = _session.query(ZIP).filter(ZIP.pincode == str(zip_code))
+		for i in results:
+			retval.append(i.as_dict())
+		return dumps(retval)
+	else:
+		res =  _session.query(ZIP).filter(ZIP.pincode == str(zip_code)).first()
+		if res is not None:
+			return dumps(res.as_dict())
+		else:
+			return None
+
+
+def encode(district, all_results=False):
+	'''
+		returns ZIP and officename for a districtname
+	'''
+	if all_results:
+		retval = []
+		for i in _session.query(ZIP).filter(ZIP.districtname==district):
+			i = i.as_dict()
+			retval.append({'pincode':i['pincode'],'officename':i['officename']})
+		return dumps(retval)
+	else:
+		res = _session.query(ZIP).filter(ZIP.districtname==district).first()
+		if res is not None:
+			res = res.as_dict()
+			return dumps({'pincode':res['pincode'],'officename':res['officename']})
+		else:
+			return None
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+setup(
+  name = 'PyZIPIN',
+  packages = ['PyZIPIN'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A python3 library providing information of ZIP codes of India based on data from data.gov.in(2018)',
+  license='MIT',
+  author = 'Ashish Shukla',
+  author_email = 'ash2shukla@gmail.com',
+  url = 'https://github.com/ash2shukla/PyZIPIN', # use the URL to the github repo
+  download_url = 'https://github.com/ash2shukla/RailIN/archive/0.1.tar.gz',
+  keywords = ['zipcodes','pincode','india','data.gov.in'], # arbitrary keywords
+  classifiers = [],
+  install_requires=[
+         'sqlalchemy'
+          ]
+)
